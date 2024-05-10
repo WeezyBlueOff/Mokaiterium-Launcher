5a2b47eb593f679209e7a3cd8529f12c3d2ab7b0
WeezyBlueOff <matthidou34@gmail.com>
2024-05-10 22:25:53 +0200

    - Changement ds Splashes a la vérification des mises a jours

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -30,9 +30,7 @@ class Splash {
 
     async startAnimation() {
         let splashes = [
-            { "message": "Je... vie...", "author": "Luuxis" },
-            { "message": "Salut je suis du code.", "author": "Luuxis" },
-            { "message": "Linux n'est pas un os, mais un kernel.", "author": "Luuxis" }
+            { "message": "Bienvenue sur Mokaiterium !", "author": "WeezyBlue" },
         ];
         let splash = splashes[Math.floor(Math.random() * splashes.length)];
         this.splashMessage.textContent = splash.message;
